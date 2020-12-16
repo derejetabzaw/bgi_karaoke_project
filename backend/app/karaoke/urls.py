2ae00adfea4a35defdc1beba427767f349ef92bf
unknown <derejetabzaw@gmail.com>
2020-12-15 16:38:45 -0800

    modified some urls

--- backend/app/karaoke/urls.py
+++ backend/app/karaoke/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	url(r'^index/$', views.index, name='index'),
     url(r'^recordings/$', views.voice_request, name='recordings')
 ]
